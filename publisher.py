39ed24aa1155a19a5afcd68fb23de489492d3b78
Thomas Farias <thomasfarias19@gmail.com>
2025-10-16 15:47:48 -0300

    feat: implemeting publisher

---
+++ publisher.py
@@ -0,0 +1,37 @@
+import pika
+import json
+
+class RabbitMQPublisher:
+    def __init__(self) -> None:
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = "guest"
+        self.__password = "guest"
+        self.__exchange = "minha_exchange"
+        self.__routing_key = ""
+        self.__channel = self.create_channel()
+
+    def create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password
+            )
+        )
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        return channel
+
+    def send_message(self, body: dict):
+        self.__channel.basic_publish(
+            exchange=self.__exchange,
+            routing_key=self.__routing_key,
+            body=json.dumps(body),
+            properties=pika.BasicProperties(
+                delivery_mode=2
+            )
+        )
+
+rabbit_mq_publisher = RabbitMQPublisher()
+rabbit_mq_publisher.send_message({ "msg": "Estou no publisher" })
